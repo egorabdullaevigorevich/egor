917399759d99737a9b386888e1a84cabca610db0
egorabdullaevigorevich <egor135eggwabd@gmail.com>
2026-09-24 12:32:04 +0500

    update

--- api/Program.cs
+++ api/Program.cs
@@ -3,10 +3,19 @@ using ListingsApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors();                      // разрешение для фронтенда
+builder.Services.AddOpenApi();                   // ← ДОБАВЬТЕ ЭТУ СТРОКУ (Служба генерации документации)
 builder.Services.AddSingleton<ListingStore>();
 
 var app = builder.Build();
 app.UseCors(p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
-app.MapListingEndpoints(); // ← Этот метод сам подключит все нужные эндпоинты из ListingEndpoints.cs
+app.MapListingEndpoints(); 
+
+app.MapOpenApi();          // документ: /openapi/v1.json
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", "Listings API v1");
+    options.RoutePrefix = "swagger";      // страница будет открываться по адресу /swagger
+});
 
 app.Run();
